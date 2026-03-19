761ad4a5362899241743ac5e34b1e726dbacd4e5
bishaldsrija08 <bishaldsrijal1@gmail.com>
2026-03-19 19:24:55 +0545

    Refactor code structure for improved readability and maintainability

--- pointer/q3.c
+++ pointer/q3.c
@@ -1,1 +1,20 @@
 // Write a function to calculate the sum, product & average of 2 numbers. Print that average in the main function.
+#include<stdio.h>
+void doSth(int a, int b, int *sum, int *product, int *average);
+int main(){
+    int a, b, sum, product, average;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    doSth(a, b, &sum, &product, &average);
+    printf("Sum: %d\n", sum);
+    printf("Product: %d\n", product);
+    printf("Average: %d\n", average);
+    return 0;
+}
+
+
+void doSth(int a, int b, int *sum, int *product, int *average){
+    *sum = a + b;
+    *product = a * b;
+    *average = (*sum) / 2;
+}
